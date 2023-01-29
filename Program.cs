714e27b281764648d617cddfc2dff0fdce4d9777
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 22:40:50 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int minutesOfReceipt = 10;
+int minutesInHour = 60;
+Console.WriteLine("Сколько человек у вас в очереди?");
+int personsInTurn = Convert.ToInt32(Console.ReadLine());
+int timeInTurn = personsInTurn * minutesOfReceipt;
+int minutes = timeInTurn % minutesInHour;
+int hours = timeInTurn / minutesInHour;
+Console.WriteLine("Вы должны стоять в очереди " + hours + " часов и " + minutes + " минут.");
